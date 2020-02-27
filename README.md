1c58d9b15db0e17a9bda47d66205223729f4599a
solmann <DanGSun@yandex.ru>
2020-02-27 15:26:36 +0300

    Initial Commit. Checkout from phil-opp's repo.

---
+++ README.md
@@ -0,0 +1,73 @@
+# Blog OS (Heap Allocation)
+
+[![Build Status](https://github.com/phil-opp/blog_os/workflows/Build%20Code/badge.svg?branch=post-10)](https://github.com/phil-opp/blog_os/actions?query=workflow%3A%22Build+Code%22+branch%3Apost-10)
+
+This repository contains the source code for the [Heap Allocation][post] post of the [Writing an OS in Rust](https://os.phil-opp.com) series.
+
+[post]: https://os.phil-opp.com/heap-allocation/
+
+**Check out the [master branch](https://github.com/phil-opp/blog_os) for more information.**
+
+## Building
+
+You need a nightly Rust compiler. First you need to install the `cargo-xbuild` and `bootimage` tools:
+
+```
+cargo install cargo-xbuild bootimage
+```
+
+Then you can build the project by running:
+
+```
+cargo xbuild
+```
+
+To create a bootable disk image, run:
+
+```
+cargo bootimage
+```
+
+This creates a bootable disk image in the `target/x86_64-blog_os/debug` directory.
+
+Please file an issue if you have any problems.
+
+## Running
+
+You can run the disk image in [QEMU] through:
+
+[QEMU]: https://www.qemu.org/
+
+```
+cargo xrun
+```
+
+Of course [QEMU] needs to be installed for this.
+
+You can also write the image to an USB stick for booting it on a real machine. On Linux, the command for this is:
+
+```
+dd if=target/x86_64-blog_os/debug/bootimage-blog_os.bin of=/dev/sdX && sync
+```
+
+Where `sdX` is the device name of your USB stick. **Be careful** to choose the correct device name, because everything on that device is overwritten.
+
+## Testing
+
+To run the unit and integration tests, execute `cargo xtest`.
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+Note that this only applies to this git branch, other branches might be licensed differently.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+use blog_os::multitasking::{self, thread::Thread, with_scheduler};
+use blog_os::{print, println};
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::allocator;
+    use blog_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    println!("Hello World{}", "!");
+    blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+    #[cfg(test)]
+    test_main();
+
+    let idle_thread = Thread::create(idle_thread, 2, &mut mapper, &mut frame_allocator).unwrap();
+    with_scheduler(|s| s.set_idle_thread(idle_thread));
+
+    for _ in 0..10 {
+        let thread = Thread::create(thread_entry, 2, &mut mapper, &mut frame_allocator).unwrap();
+        with_scheduler(|s| s.add_new_thread(thread));
+    }
+    let thread =
+        Thread::create_from_closure(|| thread_entry(), 2, &mut mapper, &mut frame_allocator)
+            .unwrap();
+    with_scheduler(|s| s.add_new_thread(thread));
+
+    println!("It did not crash!");
+    thread_entry();
+}
+
+fn idle_thread() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+        multitasking::yield_now();
+    }
+}
+
+fn thread_entry() -> ! {
+    let thread_id = with_scheduler(|s| s.current_thread_id()).as_u64();
+    for _ in 0..=thread_id {
+        print!("{}", thread_id);
+        x86_64::instructions::hlt();
+    }
+    multitasking::exit_thread();
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    unsafe {
+        blog_os::vga_buffer::WRITER.force_unlock();
+    }
+    println!("{}", info);
+    blog_os::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
